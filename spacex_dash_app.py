a65d3211ae620ea0b4f0dae71441254c469f3923
mhabibi <habibi.physics@gmail.com>
2023-11-02 16:47:49 +0100

    Add notebook for Plotly Dash

---
+++ spacex_dash_app.py
@@ -0,0 +1,97 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+app = dash.Dash(__name__)
+
+# Create an app layout
+app.layout = html.Div(children=[
+    html.H1('SpaceX Launch Records Dashboard',
+            style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+    dcc.Dropdown(id='site-dropdown',
+                 options=[
+                    {'label': 'All Sites', 'value': 'ALL'},
+                    {'label': 'CCAFS LC-40', 'value': 'CCAFS LC-40'},
+                    {'label': 'CCAFS SLC-40', 'value': 'CCAFS SLC-40'},
+                    {'label': 'KSC LC-39A', 'value': 'KSC LC-39A'},
+                    {'label': 'VAFB SLC-4E', 'value': 'VAFB SLC-4E'},
+                 ],
+                 value='ALL',
+                 placeholder="Select a Launch Site here",
+                 searchable=True
+                 ),
+    html.Br(),
+    html.Div(dcc.Graph(id='success-pie-chart')),
+    html.Br(),
+    html.P("Payload range (Kg):"),
+
+    # TASK 3: Add a Range Slider for selecting payload range
+    dcc.RangeSlider(id='payload-slider',
+                    min=0,
+                    max=10000,
+                    step=1000,
+                    marks={0: '0', 1000: '1000', 2000: '2000', 3000: '3000', 4000: '4000', 5000: '5000',
+                           6000: '6000', 7000: '7000', 8000: '8000', 9000: '9000', 10000: '10000'},
+                    value=[min_payload, max_payload]
+                    ),
+    html.Br(),
+
+    # TASK 4: Add a scatter chart to show the correlation between payload and launch success
+    html.Div(dcc.Graph(id='success-payload-scatter-chart')),
+])
+
+# TASK 2: Add a callback function to render success-pie-chart based on selected site dropdown
+@app.callback(
+    Output(component_id='success-pie-chart', component_property='figure'),
+    Input(component_id='site-dropdown', component_property='value')
+)
+def get_pie_chart(entered_site):
+    if entered_site == 'ALL':
+        fig = px.pie(spacex_df, names='Launch Site', 
+                     values='class', 
+                     title='Total Success Launches By Site')
+        return fig
+    else:
+        filtered_df = spacex_df[spacex_df['Launch Site'] == entered_site]
+        fig = px.pie(filtered_df, names='class', 
+                     title=f'Success and Failed Launches for site {entered_site}')
+        return fig
+
+# TASK 4:
+# Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+
+@app.callback(
+    Output(component_id='success-payload-scatter-chart', component_property='figure'),
+    [Input(component_id='site-dropdown', component_property='value'), 
+     Input(component_id="payload-slider", component_property="value")]
+)
+def update_scatter_chart(selected_site, payload_range):
+    filtered_df = spacex_df[(spacex_df['Payload Mass (kg)'] >= payload_range[0]) & 
+                            (spacex_df['Payload Mass (kg)'] <= payload_range[1])]
+    if selected_site == 'ALL':
+        # Display all values for Payload Mass (kg) and class
+        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class',
+                         color="Booster Version Category",
+                         title='Correlation between Payload and Success for all Launch Sites')
+    else:
+        # Display values for the selected site
+        filtered_df = filtered_df[filtered_df['Launch Site'] == selected_site]
+        fig = px.scatter(filtered_df, x='Payload Mass (kg)', y='class',
+                         color="Booster Version Category",
+                         title=f'Correlation between Payload and Success for {selected_site}')
+
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
